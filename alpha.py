d7c81323ba4c31f00e88d9aa0ec48114a2d062d4
ming018 <92374901+ming018@users.noreply.github.com>
2023-12-02 15:35:51 +0900

    환경 예제
    alpha.py에 데이터 생기는 예제 만듦

--- alpha.py
+++ alpha.py
@@ -1,9 +1,42 @@
-import numpy as np
 import random
 
-field = [ 0 for _ in range(4) ]
+percents = [ 0 for _ in range(4) ]
+field = [[0, 0] for q in range(4)]
 
 percent = random.random()
 
+
+percents[0] = 0.4
+percents[1] = 0.6
+percents[2] = 0.8
+percents[3] = 0.9
+
+reward = 0
+
 while True :
 
+    for i in range(len(percents)) :
+        if field[i][0] == 1 :
+            field[i][1] += 1
+            if field[i][1] > 3 :
+                field[i][0] = 0
+                field[i][1] = 0
+                reward -= 1
+            
+            print('요기 지나감')
+            continue
+        
+        if percent >= percents[i] :
+            field[i][0] = 1
+    
+
+    print('percent : ', percent)
+    print(field[: len(field) // 2])
+    print(field[len(field) // 2 :])
+    print(reward)
+    cmd = input()
+
+    if cmd == '.' :
+        break
+
+    percent = random.random()
